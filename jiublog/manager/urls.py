78ce44b47d936b1d0db5d39f611d328bc490cb6f
zhichao Guo <cn.guozhichao@gmail.com>
2023-02-27 17:47:59 +0800

    新增后台管理视图

--- jiublog/manager/urls.py
+++ jiublog/manager/urls.py
@@ -1,4 +1,9 @@
 from jiublog.manager import manager
-from jiublog.manager.views import HomeView
+from jiublog.manager.views import HomeView, NewBlogView, BlogsView, BlogTypeView, NewPhotoView, PhotosView
 
 manager.add_url_rule('/manager', view_func=HomeView.as_view('home'))
+manager.add_url_rule('/m/blog_new', view_func=NewBlogView.as_view('blog_new'))
+manager.add_url_rule('/m/blogs', view_func=BlogsView.as_view('blogs'))
+manager.add_url_rule('/m/blog_type', view_func=BlogTypeView.as_view('blog_type'))
+manager.add_url_rule('/m/photo_new', view_func=NewPhotoView.as_view('photo_new'))
+manager.add_url_rule('/m/photos', view_func=PhotosView.as_view('photos'))
--- jiublog/manager/views.py
+++ jiublog/manager/views.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 
 manager = Blueprint('manager', __name__)
 
+
 class HomeView(MethodView):
     """
     后台主页视图
@@ -10,6 +11,66 @@ class HomeView(MethodView):
 
     def get(self):
         return render_template('manager/index.html')
+
+    def post(self):
+        pass
+
+
+class NewBlogView(MethodView):
+    """
+    新增博客视图
+    """
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class BlogsView(MethodView):
+    """
+    管理博客视图
+    """
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class BlogTypeView(MethodView):
+    """
+    管理博客分类视图
+    """
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class NewPhotoView(MethodView):
+    """
+    新增图片视图
+    """
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class PhotosView(MethodView):
+    """
+    管理图片视图
+    """
+
+    def get(self):
+        pass
 
     def post(self):
         pass
